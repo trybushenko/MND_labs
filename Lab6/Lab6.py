0342dc53b322d32284da4f30fdfd656a78ad34ea
Artem <tribushenko02@gmail.com>
2021-05-24 18:44:47 +0300

    finished the sixth lab in mnd

--- Lab6/Lab6.py
+++ Lab6/Lab6.py
@@ -3,34 +3,33 @@ from random import randrange
 import numpy as np
 from numpy.linalg import solve
 from scipy.stats import f, t
+from prettytable import PrettyTable
 
 
-for i in range(10):
-    m = 3
-    n = 15
 
-    # варіант 327
-    x1min = -10
-    x1max = 60
-    x2min = -35
-    x2max = 10
-    x3min = -30
-    x3max = 45
+x1min = 10
+x1max = 60
+x2min = -35
+x2max = 10
+x3min = -30
+x3max = 45
 
-
-    def function(X1, X2, X3):
-        y = 6.7 + 2.0 * X1 + 2.4 * X2 + 1.6 * X3 + 5.7 * X1 * X1 + 0.7 * X2 * X2 + 2.6 * X3 * X3 + 8.7 * X1 * X2 + \
-            0.8 * X1 * X3 + 3.1 * X2 * X3 + 1.3 * X1 * X2 * X3 + randrange(0, 10) - 5
-        return y
+x01 = (x1max + x1min) / 2
+x02 = (x2max + x2min) / 2
+x03 = (x3max + x3min) / 2
+deltax1 = x1max - x01
+deltax2 = x2max - x02
+deltax3 = x3max - x03
 
 
-    x01 = (x1max + x1min) / 2
-    x02 = (x2max + x2min) / 2
-    x03 = (x3max + x3min) / 2
-    deltax1 = x1max - x01
-    deltax2 = x2max - x02
-    deltax3 = x3max - x03
-    # матриця ПФЕ
+def function(X1, X2, X3):
+    y = 6.7 + 2.0 * X1 + 2.4 * X2 + 1.6 * X3 + 5.7 * X1 * X1 + 0.7 * X2 * X2 + 2.6 * X3 * X3 + 8.7 * X1 * X2 + \
+        0.8 * X1 * X3 + 3.1 * X2 * X3 + 1.3 * X1 * X2 * X3
+    return y
+
+def main(n,m):
+    global coefs_count
+
     xn = [[-1, -1, -1, +1, +1, +1, -1, +1, +1, +1],
           [-1, -1, +1, +1, -1, -1, +1, +1, +1, +1],
           [-1, +1, -1, -1, +1, -1, +1, +1, +1, +1],
@@ -53,10 +52,13 @@ for i in range(10):
           x02, x02, x02]
     x3 = [x3min, x3max, x3min, x3max, x3min, x3max, x3min, x3max, x03, x03, x03, x03, -1.73 * deltax3 + x03,
           1.73 * deltax3 + x03, x03]
-    # заповнення нулями х1х2, х1х3, х1х2х3
-    # заповнення нулями х1kv, х2kv, х3kv
-    x1x2, x1x3, x2x3, x1x2x3 = [0] * n, [0] * n, [0] * n, [0] * n
-    x1kv, x2kv, x3kv = [0] * n, [0] * n, [0] * n
+    x1x2 = [0] * 15
+    x1x3 = [0] * 15
+    x2x3 = [0] * 15
+    x1x2x3 = [0] * 15
+    x1kv = [0] * 15
+    x2kv = [0] * 15
+    x3kv = [0] * 15
     for i in range(15):
         x1x2[i] = x1[i] * x2[i]
         x1x3[i] = x1[i] * x3[i]
@@ -65,34 +67,37 @@ for i in range(10):
         x1kv[i] = x1[i] ** 2
         x2kv[i] = x2[i] ** 2
         x3kv[i] = x3[i] ** 2
-    # формуємо список a
+
     list_for_a = list(zip(x1, x2, x3, x1x2, x1x3, x2x3, x1x2x3, x1kv, x2kv, x3kv))
 
+    for i in range(len(list_for_a)):
+        list_for_a[i] = list(list_for_a[i])
+        for j in range(len(list_for_a[i])):
+            list_for_a[i][j] = round(list_for_a[i][j], 3)
+
+
+    planning_matrix_x = PrettyTable()
+    planning_matrix_x.field_names = ['X1', 'X2', 'X3', 'X1X2', 'X1X3', 'X2X3', 'X1X2X3', 'X1X1', 'X2X2', 'X3X3']
     print("Матриця планування з натуралізованими коефіцієнтами X:")
-    print("      X1           X2           X3          X1X2         X1X3         X2X3        X1X2X3       X1X1"
-          "         X2X2         X3X3")
-    for i in range(n):
-        print(end=' ')
-        for j in range(len(list_for_a[0])):
-            print("{:^12.3f}".format(list_for_a[i][j]), end=' ')
-        print("")
-    # вивід матриці планування
+    planning_matrix_x.add_rows(list_for_a)
+    print(planning_matrix_x)
+
+
     Y = [[function(list_for_a[j][0], list_for_a[j][1], list_for_a[j][2]) for i in range(m)] for j in range(15)]
+
+    planing_matrix_y = PrettyTable()
+    planing_matrix_y.field_names = ['Y1', 'Y2', 'Y3']
     print("Матриця планування Y:")
-    print("      Y1           Y2           Y3")
-    for i in range(n):
-        print(end=' ')
-        for j in range(len(Y[0])):
-            print("{:^12.3f}".format(Y[i][j]), end=' ')
-        print("")
-    # середні у
+    planing_matrix_y.add_rows(Y)
+    print(planing_matrix_y)
+
     Y_average = []
     for i in range(len(Y)):
         Y_average.append(np.mean(Y[i], axis=0))
     print("Середні значення відгуку за рядками:")
-    for i in range(n):
+    for i in range(15):
         print("{:.3f}".format(Y_average[i]), end=" ")
-    # розрахунок дисперсій
+
     dispersions = []
     for i in range(len(Y)):
         a = 0
@@ -103,23 +108,23 @@ for i in range(10):
 
     def find_known(num):
         a = 0
-        for j in range(n):
-            a += Y_average[j] * list_for_a[j][num - 1] / n
+        for j in range(15):
+            a += Y_average[j] * list_for_a[j][num - 1] / 15
         return a
 
 
     def a(first, second):
         a = 0
-        for j in range(n):
-            a += list_for_a[j][first - 1] * list_for_a[j][second - 1] / n
+        for j in range(15):
+            a += list_for_a[j][first - 1] * list_for_a[j][second - 1] / 15
         return a
 
 
-    my = sum(Y_average) / n
+    my = sum(Y_average) / 15
     mx = []
     for i in range(10):
         number_lst = []
-        for j in range(n):
+        for j in range(15):
             number_lst.append(list_for_a[j][i])
         mx.append(sum(number_lst) / len(number_lst))
 
@@ -142,18 +147,17 @@ for i in range(10):
     beta = solve(det1, det2)
     print("\nОтримане рівняння регресії:")
     print("{:.3f} + {:.3f} * X1 + {:.3f} * X2 + {:.3f} * X3 + {:.3f} * Х1X2 + {:.3f} * Х1X3 + {:.3f} * Х2X3"
-          "+ {:.3f} * Х1Х2X3 + {:.3f} * X11^2 + {:.3f} * X22^2 + {:.3f} * X33^2 = ŷ"
-          .format(beta[0], beta[1], beta[2], beta[3], beta[4], beta[5], beta[6], beta[7], beta[8], beta[9], beta[10]))
-    y_i = [0] * n
+              "+ {:.3f} * Х1Х2X3 + {:.3f} * X11^2 + {:.3f} * X22^2 + {:.3f} * X33^2 = ŷ"
+              .format(*beta))
+    y_i = [0] * 15
     print("Експериментальні значення:")
-    for k in range(n):
+    for k in range(15):
         y_i[k] = beta[0] + beta[1] * list_for_a[k][0] + beta[2] * list_for_a[k][1] + beta[3] * list_for_a[k][2] + \
                  beta[4] * list_for_a[k][3] + beta[5] * list_for_a[k][4] + beta[6] * list_for_a[k][5] + beta[7] * \
-                 list_for_a[k][6] + beta[8] * list_for_a[k][7] + beta[9] * list_for_a[k][8] + beta[10] * list_for_a[k][
-                     9]
-    for i in range(n):
+                 list_for_a[k][6] + beta[8] * list_for_a[k][7] + beta[9] * list_for_a[k][8] + beta[10] * list_for_a[k][9]
+    for i in range(15):
         print("{:.3f}".format(y_i[i]), end=" ")
-    print("\n------------------------------- Перевірка за критерієм Кохрена -------------------------------")
+    print("\n\n------------------------------- Перевірка за критерієм Кохрена -------------------------------")
     Gp = max(dispersions) / sum(dispersions)
     Gt = 0.3346
     print("Gp =", Gp)
@@ -162,12 +166,10 @@ for i in range(10):
     else:
         print("Дисперсія неоднорідна")
 
-
-
-
-    print("------------------ Перевірка значущості коефіцієнтів за критерієм Стьюдента ------------------")
+    print("\n------------------ Перевірка значущості коефіцієнтів за критерієм Стьюдента ------------------")
     sb = sum(dispersions) / len(dispersions)
-    sbs = (sb / (n * m)) ** 0.5
+    sbs = (sb / (15 * m)) ** 0.5
+
     F3 = (m - 1) * n
     coefs1 = []
     coefs2 = []
@@ -187,25 +189,34 @@ for i in range(10):
             d -= 1
         else:
             coefs1.append(beta[j])
+    coefs_count += len(coefs1)
     print("Значущі коефіцієнти регресії:", [round(i, 3) for i in coefs1])
     print("Незначущі коефіцієнти регресії:", [round(i, 3) for i in coefs2])
     y_st = []
-    for i in range(n):
+    for i in range(15):
         y_st.append(res[0] + res[1] * x1[i] + res[2] * x2[i] + res[3] * x3[i] + res[4] * x1x2[i] + res[5] *
                     x1x3[i] + res[6] * x2x3[i] + res[7] * x1x2x3[i] + res[8] * x1kv[i] + res[9] *
                     x2kv[i] + res[10] * x3kv[i])
     print("Значення з отриманими коефіцієнтами:")
-    for i in range(n):
+    for i in range(15):
         print("{:.3f}".format(y_st[i]), end=" ")
 
-    print("\n------------------------- Перевірка адекватності за критерієм Фішера -------------------------")
-    Sad = m * sum([(y_st[i] - Y_average[i]) ** 2 for i in range(n)]) / (n - d)
+    print("\n\n------------------------- Перевірка адекватності за критерієм Фішера -------------------------")
+    Sad = m * sum([(y_st[i] - Y_average[i]) ** 2 for i in range(15)]) / (n - d)
     Fp = Sad / sb
     F4 = n - d
     print("Fp =", Fp)
-    start_3 = 0
     if Fp < f.ppf(q=0.95, dfn=F4, dfd=F3):
         print("Рівняння регресії адекватне при рівні значимості 0.05")
     else:
         print("Рівняння регресії неадекватне при рівні значимості 0.05")
 
+coefs_count = 0
+iterations_count = 0
+while coefs_count < 50:
+    main(15,3)
+    if coefs_count < 50:
+        iterations_count += 1
+
+print("Кількість значимих коефіціентів: ", coefs_count)
+print("Кількість ітерацій за які досягли 50+ значимих коефіціентів: ", iterations_count)
